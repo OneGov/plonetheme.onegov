bf6355f0ecf903f88a78bac457fcd9b847deb31d
Julian Infanger <j.infanger@4teamwork.ch>
2013-08-12 10:32:10 +0200

    Make the js file more readable.

--- plonetheme/onegov/resources/js/onegov.js
+++ plonetheme/onegov/resources/js/onegov.js
@@ -3,10 +3,12 @@ jQuery(function($) {
     e.preventDefault();
     var me = $(this);
     var parent = me.parent('li');
+
     // hide all but this children
     var others = $('#portal-globalnav li').not('#'+parent.attr('id'));
     others.find('ul').hide();
     others.removeClass('flyoutActive');
+
     var children = parent.find('ul:first');
     if (children.length == 0) {
       $.ajax({
