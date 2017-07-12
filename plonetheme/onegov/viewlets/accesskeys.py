d19fa09c7e39991c32ac315f3d164cb71116fe05
Martin Bächtold <martin@baechtold.me>
2017-07-12 09:00:18 +0200

    Refactor accesskeys viewlet.
    In Plone 4, this was based on the viewlet "plone.skip_links" which was removed in "plone.app.layout" 2.5.8 (see https://github.com/plone/plone.app.layout/blob/master/CHANGES.rst#258-2015-07-18).

--- plonetheme/onegov/viewlets/accesskeys.py
+++ plonetheme/onegov/viewlets/accesskeys.py
@@ -1,13 +1,19 @@
 from plone import api
-from plone.app.layout.viewlets import common
+from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
 
 
-class OneGovSkipLinksViewlet(common.SkipLinksViewlet):
+class OneGovAccessKeysViewlet(ViewletBase):
     index = ViewPageTemplateFile('accesskeys.pt')
 
     def update(self):
-        super(OneGovSkipLinksViewlet, self).update()
+        super(OneGovAccessKeysViewlet, self).update()
+
         nav_root = api.portal.get_navigation_root(self.context)
         self.nav_root_url = nav_root.absolute_url()
         self.nav_root_title = nav_root.Title()
+
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        self.current_page_url = context_state.current_page_url
