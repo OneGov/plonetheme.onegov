9d04b66d27128b09247bfd64f33bafbcd82f7b9c
Martin Bächtold <martin@baechtold.me>
2017-07-18 17:53:30 +0200

    Disable personal toolbar for authenticated users.
    Plone 5 provides a personal section at the bottom of the left side toolbar for authenticated users.

--- plonetheme/onegov/viewlets/personal_bar.py
+++ plonetheme/onegov/viewlets/personal_bar.py
@@ -1,3 +1,4 @@
+from plone import api
 from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
@@ -7,6 +8,14 @@ class PersonalBarViewlet(common.PersonalBarViewlet):
     """Personal bar viewlet showing the user portrait image.
     """
     index = ViewPageTemplateFile('personal_bar.pt')
+
+    def render(self):
+        # Render the personal toolbar for anonymous users only. Plone 5 provides
+        # a personal section at the bottom of the left side toolbar for
+        # authenticated users.
+        if api.user.is_anonymous():
+            return super(PersonalBarViewlet, self).render()
+        return ''
 
     def portrait_url(self):
         mtool = getToolByName(self.context, 'portal_membership')
